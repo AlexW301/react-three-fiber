39f20689c20291b225c288e0f4c50d867d4c940a
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2022-05-21 15:40:13 -0400

    Update Ball.js

--- components/react-three-fiber/Ball.js
+++ components/react-three-fiber/Ball.js
@@ -4,16 +4,13 @@ import { useFrame } from "@react-three/fiber";
 
 const Ball = () => {
   const [ref, api] = useSphere(() => ({ mass: 1, position: [0, 5, 0]}));
-  console.log(api)
+  const force = new THREE.Vector3(100, 0, 0)
+  const position = new THREE.Vector3(0, 5, 0)
   useFrame(() => {
-    // api.velocity.set(1 * 10, 0, 0)
+    // console.log(api.applyForce([0, 1, 1], [0, 0, 0]))
   })
-  console.log(api)
-  // const force = new THREE.Vector3(100, 0, 0)
-  // const position = new THREE.Vector3(0, 5, 0)
-  // api.applyImpulse(force, position)
   return (
-    <mesh position={[5, 0, 0]} ref={ref}>
+    <mesh onClick={(event) => { api.applyImpulse([5, 0, 1], [0, 0, 0]) }} position={[5, 0, 0]} ref={ref}>
       <sphereGeometry args={[1, 16, 16]}></sphereGeometry>
       <meshStandardMaterial color={"red"}></meshStandardMaterial>
     </mesh>
