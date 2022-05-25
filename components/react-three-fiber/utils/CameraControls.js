54ddce11f1a8b0b204a1df55c0cd25a665417e95
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2022-05-25 10:29:24 -0400

    Update CameraControls.js

--- components/react-three-fiber/utils/CameraControls.js
+++ components/react-three-fiber/utils/CameraControls.js
@@ -6,20 +6,6 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // Extend will make OrbitControls available as a JSX element called orbitControls for us to use.
 extend({ OrbitControls });
 
-// const CameraControls = () => {
-//   const {
-//     camera,
-//     gl: { domElement },
-//   } = useThree();
-
-//   const controls = useRef();
-//   return (
-//     <FirstPersonControls lookVertical={false} args={[camera, domElement]} onClick={() => {console.log('res')}} />
-//   );
-// };
-
-// export default CameraControls;
-
 const CameraControls = () => {
   const {
     camera,
@@ -27,14 +13,28 @@ const CameraControls = () => {
   } = useThree();
 
   const controls = useRef();
-  useFrame((state) => controls.current.update());
   return (
-    <orbitControls
-      ref={controls}
-      args={[camera, domElement]}
-      enableDamping={true}
-    />
+    <FirstPersonControls lookVertical={false} args={[camera, domElement]} onClick={() => {console.log('res')}} />
   );
 };
 
 export default CameraControls;
+
+// const CameraControls = () => {
+//   const {
+//     camera,
+//     gl: { domElement },
+//   } = useThree();
+
+//   const controls = useRef();
+//   useFrame((state) => controls.current.update());
+//   return (
+//     <orbitControls
+//       ref={controls}
+//       args={[camera, domElement]}
+//       enableDamping={true}
+//     />
+//   );
+// };
+
+// export default CameraControls;
